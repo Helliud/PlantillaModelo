78f0ef6d602b4ee182f2b866aded738b9c547fc2
Javier Eliud Lizarraga Martin Del Campo <jelmdc@hotmail.com>
2019-04-01 18:52:49 -0700

    GLM

--- PlantillaOpenGL/Modelo.cpp
+++ PlantillaOpenGL/Modelo.cpp
@@ -6,7 +6,9 @@ Modelo::Modelo() {
 
 }
 
-void Modelo::inicializarVertexArray(GLuint posicionID, GLuint colorID) {
+void Modelo::inicializarVertexArray(GLuint posicionID, GLuint colorID, GLuint transformacionesID) {
+	this->transformacionesID = transformacionesID;
+
 	//Crear un vertex array
 	glGenVertexArrays(1, &vertexArrayID);
 	glBindVertexArray(vertexArrayID);
@@ -23,7 +25,7 @@ void Modelo::inicializarVertexArray(GLuint posicionID, GLuint colorID) {
 	glEnableVertexAttribArray(posicionID);
 	glEnableVertexAttribArray(colorID);
 	//Especificar a OpenGL como se va a comunicar
-	glVertexAttribPointer(posicionID, 3, GL_FLOAT, GL_FALSE, sizeof(Vertice), 0);
+	glVertexAttribPointer(posicionID, 4, GL_FLOAT, GL_FALSE, sizeof(Vertice), 0);
 	glVertexAttribPointer(colorID, 4, GL_FLOAT, GL_FALSE, sizeof(Vertice), (void*)sizeof(vec3));
 
 	//Vertex array y buffer para el cuadrado
@@ -37,13 +39,14 @@ void Modelo::inicializarVertexArray(GLuint posicionID, GLuint colorID) {
 	glEnableVertexAttribArray(posicionID);
 	glEnableVertexAttribArray(colorID);
 	//Especificar a OpenGL como se va a comunicar
-	glVertexAttribPointer(posicionID, 3, GL_FLOAT, GL_FALSE, sizeof(Vertice), 0);
-	glVertexAttribPointer(colorID, 4, GL_FLOAT, GL_FALSE, sizeof(Vertice), (void*)sizeof(vec3));
+	glVertexAttribPointer(posicionID, 4, GL_FLOAT, GL_FALSE, sizeof(Vertice), 0);
+	glVertexAttribPointer(colorID, 4, GL_FLOAT, GL_FALSE, sizeof(Vertice), (void*)sizeof(vec4));
 }
 
 void Modelo::dibujar(GLenum	modoDibujo) {
 	shader->enlazar();
 	glBindVertexArray(vertexArrayID);
+	glUniformMatrix4fv(transformacionesID, 1, GL_FALSE, &transformaciones[0][0]);
 	glDrawArrays(modoDibujo, 0, vertices.size());
 	glBindVertexArray(0);
 	shader->desenlazar();
